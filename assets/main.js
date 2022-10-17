c570e6b37e718201ecb1b79f331affbdbc1eb516
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-17 16:24:40 +0200

    bonus non completo

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,10 @@
 
 
 // selexione il bottone
-let bottone = document.querySelector(".btn")
+let bottone1 = document.querySelector(".aqua")
 
 // assegno un azione al bottone
-bottone.addEventListener("click", function(){
+bottone1.addEventListener("click", function(){
 
     // // struttura del container
     // let container = document.querySelector(".container")
@@ -49,6 +49,95 @@ bottone.addEventListener("click", function(){
 
 
             let casella =`<div class="casella"> ${i}  </div>`;
+            containerEl.innerHTML += casella;
+
+            // casellaMarkUp.innerText += numero_caselle;
+        }
+        
+    }
+
+
+
+
+
+
+
+    
+})
+
+
+
+
+// selexione il bottone
+let bottone2 = document.querySelector(".violet")
+
+// assegno un azione al bottone
+bottone2.addEventListener("click", function(){
+
+
+
+   // struttura del container
+    let container = document.querySelector(".container")
+
+    // assegno un numero alle caselle
+    let numero_caselle = 82;
+    
+    // richiamo la function
+    casellegenerator(numero_caselle, container);
+    
+
+    // funzione che genera le caselle
+    function casellegenerator(numero_caselle, containerEl) {
+        
+        for (let i = 1; i < numero_caselle; i++) {
+
+
+            let casella =`<div class="casella_2"> ${i}  </div>`;
+            containerEl.innerHTML += casella;
+
+            // casellaMarkUp.innerText += numero_caselle;
+        }
+        
+    }
+
+
+
+
+
+
+
+    
+})
+
+
+
+
+// selexione il bottone
+let bottone3 = document.querySelector(".green")
+
+// assegno un azione al bottone
+bottone3.addEventListener("click", function(){
+
+
+
+
+   // struttura del container
+    let container = document.querySelector(".container")
+
+    // assegno un numero alle caselle
+    let numero_caselle = 50;
+    
+    // richiamo la function
+    casellegenerator(numero_caselle, container);
+    
+
+    // funzione che genera le caselle
+    function casellegenerator(numero_caselle, containerEl) {
+        
+        for (let i = 1; i < numero_caselle; i++) {
+
+
+            let casella =`<div class="casella_3"> ${i}  </div>`;
             containerEl.innerHTML += casella;
 
             // casellaMarkUp.innerText += numero_caselle;
